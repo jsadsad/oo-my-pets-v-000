79d50a51131d834963af78ab4e25ab56c906308d
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-06-15 07:58:28 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -40,11 +40,11 @@ class Owner
   end
   
   def sell_pets
-    pets.each do |species, animal|
+    pets.each do |species, animals|
       animals.each do |animal|
         animal.mood = 'nervous'
       end
-      
+      animal.clear
   end
   
   def list_pets
