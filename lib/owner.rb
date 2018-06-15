09bd6d69f2e024b86a0c2dcc9f9a3a080c1100d1
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-06-15 07:52:22 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :name
+  
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+  
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+  
+  def buy_dog(name)
+    pets[:dog] << Dog.new(name)
+  end
+  
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+  end
+  
 end
